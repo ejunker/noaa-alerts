7a3afb77c02d37bece85902eda32d15fffc20cc3
Eric Junker <eric@flaggpole.com>
2012-11-05 21:47:01 -0600

    Add missing alert elements

--- lib/alert.rb
+++ lib/alert.rb
@@ -2,37 +2,77 @@ require "noaa-alerts/version"
 
 module Noaa
   class Alert
-    attr_reader :url, :description, :locations, :identifier, :effective_at, :expires_at, :event, :urgency, :severity, :headline, :sent_at
+    attr_reader :url, :identifier, :sender, :sent_at, :status, :msg_type, :scope, :note,
+      :category, :event, :urgency, :severity, :certainty, :event_codes, :effective_at, :expires_at, :sender_name, :headline, :description, :instruction, :parameters,
+      :area_desc, :polygon, :locations, :geocodes
 
     def initialize(url, entry)
       @url = url || ""
-      @description = ""
+
+      # alert
+      @identifier = ""
+      @sender = ""
+      @sent_at = nil
+      @status = ""
+      @msg_type = ""
+      @scope = ""
+      @note = ""
+
+      # info
+      @category = ""
       @event = ""
       @urgency = ""
       @severity = ""
-      @headline = ""
-      @locations = []
-      @identifier = ""
+      @certainty = ""
+      @event_codes = {}
       @effective_at = nil
       @expires_at = nil
-      @sent_at = nil
+      @sender_name = ""
+      @headline = ""
+      @description = ""
+      @instruction = ""
+      @parameters = {}
+
+      # area
+      @area_desc = ""
+      @polygon = ""
+      @locations = []
+      @geocodes = {}
+
       handle_entry(entry) if entry
     end
 
     private
     
     def handle_entry(entry)
+      @identifier = entry.fetch('identifier')
+      @sender = entry.fetch('sender')
+      @sent_at = Time.parse(entry.fetch('sent'))
+      @status = entry.fetch('status')
+      @msg_type = entry.fetch('msgType')
+      @scope = entry.fetch('scope')
+      @note = entry.fetch('note')
+
       info = entry.fetch('info')
-      @description = info.fetch('description')
+      @category = info.fetch('category')
       @event = info.fetch('event')
       @urgency= info.fetch('urgency')
       @severity = info.fetch('severity')
-      @headline = info.fetch('headline')
-      @locations = info.fetch('area').fetch('areaDesc').split('; ')
-      @identifier = entry.fetch('identifier')
-      @sent_at = Time.parse(entry.fetch('sent'))
+      @certainty = info.fetch('certainty')
+      @event_codes = info.fetch('eventCode')
       @effective_at = Time.parse(info.fetch('effective'))
       @expires_at = Time.parse(info.fetch('expires'))
+      @sender_name = info.fetch('senderName')
+      @headline = info.fetch('headline')
+      @description = info.fetch('description')
+      @instruction = info.fetch('instruction')
+      @parameters = info.fetch('parameter')
+
+      area = info.fetch('area')
+      @area_desc = area.fetch('areaDesc')
+      @polygon = area.fetch('polygon')
+      @locations = @area_desc.split('; ')
+      @geocodes = area.fetch('geocode')
     end
   end
 end
